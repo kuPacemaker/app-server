de2725320efdf3e10942a3ff0b884fe7b568fec5
SeongChan <smc5407@gmail.com>
2020-10-28 02:21:19 +0900

    add refresh, two requests and modify model4

--- polls/FeedManager/FeedManager.py
+++ polls/FeedManager/FeedManager.py
@@ -60,7 +60,7 @@ class feedManager():
             data["board"]["leader"][i]["image"] = None
 
         guests = Guest.objects.filter(user = user)
-        guest_channel = Guest.objects.filter(id__in = guest.values_list('channel', flat=True))
+        guest_channel = Guest.objects.filter(id__in = guests.values_list('channel', flat=True))
         guest_length = guest_channel.count()
         serializer = ChannelInfoSerializer(guset_channel, many=True)
         data["board"]["runner"] = [0 for i in range(guest_length)]
