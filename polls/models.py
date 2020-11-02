818227c5b1a092e21bd9f10835a64ff86fc92e8b
SeongChan <smc5407@gmail.com>
2020-11-02 15:07:26 +0900

    add gitignore4

--- polls/models.py
+++ polls/models.py
@@ -4,12 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
-
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-
 import datetime, uuid
 
 # Create your models here.
