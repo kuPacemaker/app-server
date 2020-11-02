71b08648ca4cda6cc168fd037a47d8bc3578def6
SeongChan <smc5407@gmail.com>
2020-11-02 11:15:09 +0900

    modify AESCipher

--- polls/AESCipher.py
+++ polls/AESCipher.py
@@ -14,10 +14,10 @@ class AESCipher:
         raw = pad(raw)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw))
+        return base64.b64encode(iv + cipher.encrypt(raw)).decode('utf-8')
 
     def decrypt(self, enc):
-        enc = base64.b64decode(enc)
+        enc = base64.b64decode(enc.incode('utf-8'))
         iv = enc[:16]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return unpad(cipher.decrypt(enc[16:])).decode('utf-8')
